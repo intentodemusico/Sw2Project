5e11ce0c4a14deaf3d9ce283c28c1b96a5d2b989
? <alberto.garcia.2016@upb.edu.co>
2019-05-21 22:30:23 -0500

    Update 2019_05_22_023423_create_categories_table.php

--- project/database/migrations/2019_05_22_023423_create_categories_table.php
+++ project/database/migrations/2019_05_22_023423_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             $table->string('CategoryName');
             $table->longText('Description');
             $table->softDeletes();
+            $table->timestamps();
         });
     }
 
